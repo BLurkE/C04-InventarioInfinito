fa9705ae3dfa6c89bc85c14669ef95a3abf52cd0
Felipe-SSS <6felipe.sss@gmail.com>
2025-09-09 18:23:30 -0300

    Entrega Parcial 2 - Começamos a implementar os requisitos dessa entrega

---
+++ functions.cpp
@@ -0,0 +1,176 @@
+#include <iostream>
+using namespace std;
+
+struct Item {
+  int id;
+  string nome;
+  string dono;
+  string categoria;
+  int apego_emocional;
+};
+
+struct No
+{
+    Item item;
+    No *proximo;
+    No() : proximo(NULL) {}
+};
+
+struct Lista
+{
+    No *primeiro;
+    int totalItens;
+    Lista() : primeiro(NULL), totalItens(0) {}
+};
+
+bool listaVazia(Lista *lista)
+{
+    return lista->primeiro == NULL;
+}
+
+void append(Lista *lista, Item novoItem)
+{
+    No *novoNo = new No;
+    novoNo->item = novoItem;
+
+    if (listaVazia(lista))
+    {
+        lista->primeiro = novoNo;
+    }
+    else
+    {
+        No *atual = lista->primeiro;
+        while (atual->proximo != NULL)
+        {
+            atual = atual->proximo;
+        }
+        atual->proximo = novoNo;
+    }
+    lista->totalItens += 1;
+}
+
+bool pop(Lista *lista, string nome)
+{
+    if (listaVazia(lista))
+    {
+        cout << "Lista vazia." << endl;
+        return false;
+    }
+
+    No *atual = lista->primeiro;
+    No *anterior = NULL;
+
+    while (atual != NULL && atual->item.nome != nome)
+    {
+        anterior = atual;
+        atual = atual->proximo;
+    }
+
+    if (atual == NULL)
+    {
+        cout << "Elemento nao encontrado." << endl;
+        return false;
+    }
+
+    if (anterior == NULL)
+    {
+        lista->primeiro = atual->proximo;
+    }
+    else
+    {
+        anterior->proximo = atual->proximo;
+    }
+
+    delete atual;
+    lista->totalItens -= 1;
+    cout << "Elemento removido: " << nome << endl;
+    return true;
+}
+
+void mostraItens(Lista *lista)
+{
+    if (lista->totalItens >= 1)
+    {
+        No *atual = lista->primeiro;
+        cout << "\nItens disponiveis:\n";
+        while (atual != NULL)
+        {
+            cout << atual->item.id << endl;
+            cout << atual->item.nome << endl;
+            cout << atual->item.dono << endl;
+            cout << atual->item.categoria << endl;
+            cout << atual->item.apego_emocional << endl;
+
+            atual = atual->proximo;
+        }
+        cout << endl;
+    }
+
+    else{
+        cout << "\nAinda nao existem itens cadastrados.\n" << endl;
+    }
+}
+
+Lista ordenarItens_Nome(Lista* lista) {
+    Lista novaListaOrdenada;
+
+    No* atual = lista->primeiro;
+
+    while (atual != NULL) {
+        No* copia = new No;
+        copia->item = atual->item;
+        copia->proximo = NULL;
+
+        if (novaListaOrdenada.primeiro == NULL || 
+            copia->item.nome < novaListaOrdenada.primeiro->item.nome) {
+            
+            copia->proximo = novaListaOrdenada.primeiro;
+            novaListaOrdenada.primeiro = copia;
+        } else {
+            No* pos = novaListaOrdenada.primeiro;
+            while (pos->proximo != NULL && 
+                   pos->proximo->item.nome < copia->item.nome) {
+                pos = pos->proximo;
+            }
+            copia->proximo = pos->proximo;
+            pos->proximo = copia;
+        }
+
+        atual = atual->proximo;
+    }
+
+    novaListaOrdenada.totalItens = lista->totalItens;
+
+    return novaListaOrdenada;
+}
+
+Lista ordenarItens_Apego(Lista* lista) {
+    Lista novaListaOrdenada;
+
+    No* atual = lista->primeiro;
+
+    while (atual != NULL) {
+        No* copia = new No;
+        copia->item = atual->item;
+        copia->proximo = NULL;
+
+        if (novaListaOrdenada.primeiro == NULL || 
+            copia->item.apego_emocional < novaListaOrdenada.primeiro->item.apego_emocional) {
+            
+            copia->proximo = novaListaOrdenada.primeiro;
+            novaListaOrdenada.primeiro = copia;
+        } else {
+            No* pos = novaListaOrdenada.primeiro;
+            while (pos->proximo != NULL && 
+                   pos->proximo->item.apego_emocional < copia->item.apego_emocional) {
+                pos = pos->proximo;
+            }
+            copia->proximo = pos->proximo;
+            pos->proximo = copia;
+        }
+
+        atual = atual->proximo;
+    }
+
+    return novaListaOrdenada;
+}
--- main.cpp
+++ main.cpp
@@ -1,66 +1,162 @@
 #include <iostream>
+#include "functions.cpp"
 using namespace std;
 
-void inserirItem(){
-  cout << "Funcionalidade em construção" << endl;
+void inserirItem(Lista *lista)
+{
+  Item novoItem;
+  novoItem.id = 0;
+  novoItem.categoria = "Higiene";
+  novoItem.apego_emocional = 30;
+  novoItem.dono = "Pedro";
+  novoItem.nome = "Escova";
+  append(lista, novoItem);
+  novoItem.id = 1;
+  novoItem.categoria = "Higiene";
+  novoItem.apego_emocional = 75;
+  novoItem.dono = "Maria";
+  novoItem.nome = "Shampoo";
+  append(lista, novoItem);
+  novoItem.id = 2;
+  novoItem.categoria = "Cozinha";
+  novoItem.apego_emocional = 20;
+  novoItem.dono = "Joao";
+  novoItem.nome = "Panela";
+  append(lista, novoItem);
+  novoItem.id = 3;
+  novoItem.categoria = "Eletronicos";
+  novoItem.apego_emocional = 90;
+  novoItem.dono = "Pedro";
+  novoItem.nome = "Celular";
+  append(lista, novoItem);
+  novoItem.id = 4;
+  novoItem.categoria = "Roupas";
+  novoItem.apego_emocional = 15;
+  novoItem.dono = "Carla";
+  novoItem.nome = "Jaqueta";
+  append(lista, novoItem);
+  novoItem.id = 5;
+  novoItem.categoria = "Livros";
+  novoItem.apego_emocional = 100;
+  novoItem.dono = "Lucas";
+  novoItem.nome = "Diario";
+  append(lista, novoItem);
+  novoItem.id = 6;
+  novoItem.categoria = "Moveis";
+  novoItem.apego_emocional = 40;
+  novoItem.dono = "Ana";
+  novoItem.nome = "Cadeira";
+  append(lista, novoItem);
+  novoItem.id = 7;
+  novoItem.categoria = "Decoracao";
+  novoItem.apego_emocional = 65;
+  novoItem.dono = "Felipe";
+  novoItem.nome = "Quadro";
+  append(lista, novoItem);
+  novoItem.id = 8;
+  novoItem.categoria = "Esporte";
+  novoItem.apego_emocional = 55;
+  novoItem.dono = "Bruno";
+  novoItem.nome = "Bola de futebol";
+  append(lista, novoItem);
+  novoItem.id = 9;
+  novoItem.categoria = "Instrumentos";
+  novoItem.apego_emocional = 85;
+  novoItem.dono = "Mariana";
+  novoItem.nome = "Violao";
+  append(lista, novoItem);
+  novoItem.id = 10;
+  novoItem.categoria = "Brinquedos";
+  novoItem.apego_emocional = 10;
+  novoItem.dono = "Sofia";
+  novoItem.nome = "Boneca";
+  append(lista, novoItem);
 }
 
-void cadastrarSimilaridade(){
-  cout << "Funcionalidade em construção" << endl;
+void cadastrarSimilaridade()
+{
+  cout << "Funcionalidade em construcao" << endl;
 }
 
-void verificarExistenciaItem(){
-  cout << "Funcionalidade em construção" << endl;
+void verificarExistenciaItem()
+{
+  cout << "Funcionalidade em construcao" << endl;
 }
 
-void listarItens(){
-  cout << "Funcionalidade em construção" << endl;
+void listarItens(Lista *lista)
+{
+  mostraItens(lista);
 }
 
-void listarItensAlfabeticaNome(){
-  cout << "Funcionalidade em construção" << endl;
+void listarItensAlfabeticaNome(Lista* lista)
+{
+  Lista listaOrdenada = ordenarItens_Nome(lista);
+  mostraItens(&listaOrdenada);
 }
 
-void listarItensDecrescenteRaridade(){
-  cout << "Funcionalidade em construção" << endl;
+void listarItensDecrescenteApego()
+{
+  cout << "Funcionalidade em construcao" << endl;
 }
 
-void contarItensPropMagica(){
-  cout << "Funcionalidade em construção" << endl;
+void contarItensComMesmoApego()
+{
+  cout << "Funcionalidade em construcao" << endl;
 }
 
-void removerMenosRaros(){
-  cout << "Funcionalidade em construção" << endl;
+void removerMenosApegados()
+{
+  cout << "Funcionalidade em construcao" << endl;
 }
 
-int main() {
+int main()
+{
   int opMenu;
 
+  Lista listaDeItens;
 
-  cin >> opMenu;
+  
   /* Menu */
-  switch (opMenu){
+  while (opMenu != 0)
+  {
+
+    cin >> opMenu;
+
+    switch (opMenu)
+    {
 
     case 1:
-      inserirItem();
+      inserirItem(&listaDeItens);
+      break;
     case 2:
       cadastrarSimilaridade();
+      break;
     case 3:
       verificarExistenciaItem();
+      break;
     case 4:
-      listarItens();
+      listarItens(&listaDeItens);
+      break;
     case 5:
-      listarItensAlfabeticaNome();
+      listarItensAlfabeticaNome(&listaDeItens);
+      break;
     case 6:
-      listarItensDecrescenteRaridade();
+      listarItensDecrescenteApego();
+      break;
     case 7:
-      contarItensPropMagica();
+      contarItensComMesmoApego();
+      break;
     case 8:
-      removerMenosRaros();
+      removerMenosApegados();
+      break;
+    case 0:
+      cout << "Ate a proxima!!" << endl;
+      break;
     default:
-      cout << "Caso de exceção. Selecione uma opção válida." << endl;
+      cout << "Caso de excecao. Selecione uma opcao valida." << endl;
+      break;
+    }
   }
-
 
   return 0;
 }
